a7d0a866c2ad81d0457955e578ebf6c07800b15e
EVAN GREANEY - STUDENT <G00348451@gmit.ie>
2019-10-03 14:36:42 +0100

    lab_upload

---
+++ src/components/create.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class Create extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello from Create Component</h1>
+      </div>
+    );
+  }
+}
+
+export default Create;
